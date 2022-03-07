331c6750817ee23d5a32241feaad40653834f725
Cha Vue <chavue09@gmail.com>
2022-03-06 21:20:06 -0600

    prompt questions for manager data

--- app.js
+++ app.js
@@ -9,3 +9,64 @@ fs.writeFile('./index.html', pageHTML, err => {
 
     console.log('Team generated! Check out index.html to see output!')
 });
+
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your manager?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your manager!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Enter the manager ID.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the manager ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Enter the manager email.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the manager email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter the manager office number.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the manager office number!');
+                    return false;
+                }
+            }
+        }
+        
+    ]);
+};
+
