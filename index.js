f89fd75653a26bbc36acc8fc727282f6267cfe71
Cha Vue <chavue09@gmail.com>
2022-03-06 23:27:44 -0600

    fixed script for index

--- index.js
+++ index.js
@@ -70,24 +70,24 @@ const promptUser = () => {
     ]);
 };
 
-const promptEmployee = teamData => {
+const promptEngineer = teamData => {
     // If there's no 'Employee' array property, create one
     if (!teamData.employee) {
         teamData.employee = [];
     }
     console.log(`
     ==================
-    Add a New Employee
+    Add a New Engineer
     ==================
     `);
         return inquirer.prompt([
-            {
-                type: 'list',
-                name: 'employeePosition',
-                message: 'Would you like to add an Engineer or Intern to your team?',
-                choices: ['Engineer', 'Intern', 'Finish building my team'],
-                default: 'Finish building my team'
-            },
+            // {
+            //     type: 'list',
+            //     name: 'employeePosition',
+            //     message: 'Would you like to add an Engineer or Intern to your team?',
+            //     choices: ['Engineer', 'Intern', 'Finish building my team'],
+            //     default: 'Finish building my team'
+            // },
             {
                 type: 'input',
                 name: 'engineerName',
@@ -97,13 +97,6 @@ const promptEmployee = teamData => {
                         return true;
                     } else {
                         console.log('Please enter the name of the Engineer!');
-                        return false;
-                    }
-                },
-                when: ({ confirmEmployee }) => {
-                    if (confirmEmployee) {
-                        return true;
-                    } else {
                         return false;
                     }
                 }
@@ -119,13 +112,6 @@ const promptEmployee = teamData => {
                         console.log('Please enter the ID of the Engineer?');
                         return false;
                     }
-                },
-                when: ({ confirmEngineer }) => {
-                    if (confirmEngineer) {
-                        return true;
-                    } else {
-                        return false;
-                    }
                 }
             },
             {
@@ -137,13 +123,6 @@ const promptEmployee = teamData => {
                         return true;
                     } else {
                         console.log('Please enter the email of the Engineer');
-                        return false;
-                    }
-                },
-                when: ({ confirmEngineer }) => {
-                    if (confirmEngineer) {
-                        return true;
-                    } else {
                         return false;
                     }
                 }
@@ -159,22 +138,108 @@ const promptEmployee = teamData => {
                         console.log('Please enter the github username of the Engineer');
                         return false;
                     }
-                },
-                when: ({ confirmEngineer }) => {
-                    if (confirmEngineer) {
-                        return true;
-                    } else {
-                        return false;
-                    }
                 }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddEmployee',
+                message: 'Would you like to enter another project?',
+                default: false
             }
         ])
         .then(employeeData => {
             teamData.employee.push(employeeData);
             if (employeeData.confirmAddEmployee) {
-                return promptEmployee(teamData);
+                return promptEngineer(teamData);
             } else {
                 return teamData;
+            }
+        });
+
+}
+const promptIntern = internData => {
+    // If there's no 'Intern' array property, create one
+    if (!internData.person) {
+        internData.person = [];
+    }
+    console.log(`
+    ================
+    Add a New Intern
+    ================
+    `);
+        return inquirer.prompt([
+            // {
+            //     type: 'list',
+            //     name: 'employeePosition',
+            //     message: 'Would you like to add an Engineer or Intern to your team?',
+            //     choices: ['Engineer', 'Intern', 'Finish building my team'],
+            //     default: 'Finish building my team'
+            // },
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'What is the intern name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the name of the intern!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: 'What is the intern ID?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the ID of the intern!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'What is the intern email?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the email of the intern');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: 'What is the intern school?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the school of the intern');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddIntern',
+                message: 'Would you like to enter another project?',
+                default: false
+            }
+        ])
+        .then(personData => {
+            internData.person.push(personData);
+            if (personData.confirmAddIntern) {
+                return promptIntern(internData);
+            } else {
+                return internData;
             }
         });
 
@@ -182,7 +247,10 @@ const promptEmployee = teamData => {
 
 
 promptUser()
-    .then(promptEmployee)
+
+    .then(promptEngineer)
+    .then(promptIntern)
+    
     .then(teamData => {
         console.log(teamData);
         // const pageHTML = generatePage(engData);
@@ -190,4 +258,13 @@ promptUser()
         //   if (err) throw new Error(err);
         //   console.log('Page created! Check out index.html in this directory to see it!');
         // });
-    });
+    })
+    .then(internData => {
+        console.log(internData);
+        // const pageHTML = generatePage(engData);
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //   if (err) throw new Error(err);
+        //   console.log('Page created! Check out index.html in this directory to see it!');
+        // });
+    })
+    
